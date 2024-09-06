1ae838e7f6dc35f98de3d0e3622f2d2e0d7503af
Hassan Umar <hassan@neonscreens.com>
2024-09-06 11:24:08 +0500

    feat: google analytics

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
-import resumePDF from "../../assets/Resume.pdf";
+import resumePDF from "../../assets/hassan-umar-resume.pdf";
 
 const Hero = () => {
   return (
